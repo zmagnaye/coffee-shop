cc0c5fd75baaf9b67cf984b16529fbabd3fdaf47
Zari <zjcmagnaye@gmail.com>
2025-05-31 00:16:36 -0700

    feat: added JS functions for email validation, total calculation, random drink, order ID, and feedback

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Function for modal
 const drinkModal = document.getElementById("drinkModal");
 
 drinkModal.addEventListener("show.bs.modal", event => {
@@ -11,3 +12,47 @@ drinkModal.addEventListener("show.bs.modal", event => {
     modalTitle.textContent = title;
     modalBody.textContent = description;
 });
+
+//Function to validate email format
+function validateEmail(email){
+    return email.includes("@") && email.includes(".");
+}
+
+console.log(validateEmail("test@email.com"));
+
+//Function to calculate total
+const drinkPrices=[5.65, 5.50, 6.50];
+
+function calculateTotal(prices) {
+    return prices.reduce((total, price) => total + price, 0).toFixed(2);
+}
+
+console.log("Total Price: $", calculateTotal(drinkPrices));
+
+//Function to return a random drink from the array
+const drinks=["House Coffee", "Vietnamese Coffee", "Mint Sereno", "Mung Bean", "Thai Milk Tea"];
+
+function getRandomDrink() {
+    const index = Math.floor(Math.random() * drinks.length);
+    return drinks[index];
+}
+
+console.log("Random Drink: ", getRandomDrink());
+
+//Function to generate a random order ID
+function generateOrderID() {
+    const randomNum = Math.floor(1000 + Math.random() * 9000);
+    return "Order ID#: " + randomNum;
+}
+
+console.log(generateOrderID());
+
+//Function for adding customer feedback
+let feedbackList = [];
+
+function addFeedback(message) {
+    feedbackList.push(message);
+    return feedbackList;
+}
+
+console.log(addFeedback("House Cofee is always the best!"));
